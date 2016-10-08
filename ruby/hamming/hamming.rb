cc8acfb97a2f875ed79b0dff0828c435e97b2b05
libbyschuknight <libbysk@gmail.com>
2016-10-09 11:40:28 +1300

    4th iteration

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,17 +1,12 @@
 class Hamming
-
   def self.compute(strand_1, strand_2)
     strand_1 = strand_1.chars
     strand_2 = strand_2.chars
 
     raise ArgumentError if strand_1.length != strand_2.length
 
-    count = 0
-
-    strand_1.zip(strand_2).map do | n1, n2|
-      count += 1 unless n1 == n2
-    end
-    count
+    count = strand_1.zip(strand_2).map { |strand| strand.count(strand[0]) }
+    count.select { |e| e == 1 }.length
   end
 end
 
