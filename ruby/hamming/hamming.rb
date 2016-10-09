6ec585320417537f5701c26aa2b4c66b7e0fd859
libbyschuknight <libbysk@gmail.com>
2016-10-09 15:04:04 +1300

    5th

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,15 +1,12 @@
-class Hamming
-  def self.compute(strand_1, strand_2)
-    strand_1 = strand_1.chars
-    strand_2 = strand_2.chars
-
-    raise ArgumentError if strand_1.length != strand_2.length
-
-    count = strand_1.zip(strand_2).map { |strand| strand.count(strand[0]) }
-    count.select { |e| e == 1 }.length
-  end
-end
-
 module BookKeeping
   VERSION = 3 # Where the version number matches the one in the test.
+end
+
+class Hamming
+  def self.compute(strand_1, strand_2)
+    raise ArgumentError, "Strands are not equal in length" if strand_1.length != strand_2.length
+    strand_1 = strand_1.chars
+    strand_2 = strand_2.chars
+    strand_1.zip(strand_2).count { |nucleotide1, nucleotide2| nucleotide1 != nucleotide2 }
+  end
 end
